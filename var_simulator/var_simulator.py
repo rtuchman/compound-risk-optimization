bed18fe06f87749f44415f7055e15a6db5bf3fc8
rtuchman <runtuchman@gmail.com>
2023-05-08 15:00:15 +0300

    add docstring and update code

--- var_simulator/var_simulator.py
+++ var_simulator/var_simulator.py
@@ -2,7 +2,9 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from typing import Dict, Union
+from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
 
 import arch
@@ -12,29 +14,58 @@ CPU_COUNT = os.cpu_count()
 
 
 class VarSimulator:
-
+    """
+    A class that can simulate price trajectories for given assets and calculate Value at Risk (VaR) estimates using
+    historical data and Generalized Autoregressive Conditional Heteroskedasticity (GARCH) model. VaR is the maximum loss
+    that an investor can incur, within a certain confidence interval, over a given period of time.
+    """
     def __init__(self, df_prices: pd.DataFrame):
+        """
+         Initializes the VarSimulator object with a DataFrame of historical asset prices.
+
+         :param df_prices: A pandas DataFrame containing the historical prices of the assets to be simulated.
+
+         This method initializes the VarSimulator object by calculating the logarithmic returns of the assets in the
+         given DataFrame, and creating a list of asset names. It also initializes four dictionaries to store the GARCH
+         models, forecasts, historical volatilities, and GARCH volatilities for each asset.
+         """
         self.df_prices = df_prices
         self.returns = df_prices.pct_change().dropna()
         self.log_returns = np.log(1 + self.returns)
         self.assets = self.log_returns.columns
+        self.models = {}
+        self.forecasts = {}
+        self.historical_volatility = {}
+        self.garch_volatility = {}
 
     def calculate_volatility(self, p: int, q: int, n_tests: int) -> Union[Dict, Dict, Dict, Dict]:
-        models = {}
-        forecasts = {}
-        historical_volatility = {}
-        garch_volatility = {}
+        """
+        Calculates the GARCH volatility forecasts and historical volatilities for each asset in the dataset.
+
+        :param p: The order of the autoregressive (AR) term.
+        :param q: The order of the moving average (MA) term.
+        :param n_tests: The number of periods to be used in the rolling standard deviation calculation.
+
+        :return: A dictionary containing the GARCH models, forecasts, historical volatilities, and GARCH volatilities
+        for each asset in the dataset.
+        """
         for asset in self.assets:
             model = arch.arch_model(self.log_returns[asset], p=p, q=q, vol="Garch", dist="Normal")
-            models[asset] = model.fit(disp=False)
-            forecasts[asset] = models[asset].forecast(horizon=n_tests, method='simulation', reindex=False)
-            historical_volatility[asset] = self.log_returns[asset].rolling(window=n_tests - 1).std().iloc[-1]
-            garch_volatility[asset] = forecasts[asset].variance.iloc[-1]
-
-        return models, forecasts, historical_volatility, garch_volatility
+            self.models[asset] = model.fit(disp=False)
+            self.forecasts[asset] = self.models[asset].forecast(horizon=n_tests, method='simulation', reindex=False)
+            self.historical_volatility[asset] = self.log_returns[asset].rolling(window=n_tests - 1).std().iloc[-1]
+            self.garch_volatility[asset] = self.forecasts[asset].variance.iloc[-1]
 
     def generate_random_price_trajectories(self, forecasts: univariate.base.ARCHModelForecast,
                                            n_periods: int = 720) -> Dict:
+        """
+        Generates random price trajectories for given assets for a given number of periods, based on the GARCH forecasts.
+
+        :param forecasts: The forecast objects obtained from the GARCH model.
+        :param n_periods: The number of periods to be simulated.
+
+        :return: A dictionary containing the price trajectories for the given assets.
+        """
         self.n_periods = n_periods
         price_trajectories = {}
         for asset in self.assets:
@@ -57,6 +88,13 @@ class VarSimulator:
         return price_trajectories
 
     def _calc_user_debt(self, df_top_borrow_positions: pd.DataFrame) -> pd.DataFrame:
+        """
+        Calculates the user debt for each account based on the top borrow positions.
+
+        :param df_top_borrow_positions: A pandas DataFrame containing the top borrow positions for the accounts.
+
+        :return: A pandas DataFrame containing the user debt for each account.
+        """
         user_debt = (df_top_borrow_positions['totalUnderlyingBorrowed'] - df_top_borrow_positions[
             'totalUnderlyingRepaid']) - (df_top_borrow_positions['totalUnderlyingSupplied'] - df_top_borrow_positions[
             'totalUnderlyingRedeemed'])
@@ -64,6 +102,13 @@ class VarSimulator:
 
     def _calculate_single_var_measurement(self, df_top_borrow_positions: pd.DataFrame,
                                           forecasts: univariate.base.ARCHModelForecast, simulation_index: int):
+        """
+        Calculates the user debt for each account based on the top borrow positions.
+
+        :param df_top_borrow_positions: A pandas DataFrame containing the top borrow positions for the accounts.
+
+        :return: A pandas DataFrame containing the user debt for each account.
+        """
         price_trajectories = self.generate_random_price_trajectories(forecasts)
         hourly_user_debts = []
         for acount_id, df_account in df_top_borrow_positions.groupby('account_id'):
@@ -79,10 +124,19 @@ class VarSimulator:
 
     def calculate_var(self, df_top_borrow_positions: pd.DataFrame, forecasts: univariate.base.ARCHModelForecast,
                       n_simulations: int):
+        """
+        Calculates the VaR for given top borrow positions and the GARCH forecasts for a given number of simulations.
+
+        :param df_top_borrow_positions: A pandas DataFrame containing the top borrow positions for the accounts.
+        :param forecasts: The forecast objects obtained from the GARCH model.
+        :param n_simulations: The number of simulations to be run.
+
+        :return: None
+        """
         self.var_results = {}
         df_top_borrow_positions = df_top_borrow_positions[
             df_top_borrow_positions['symbol'].isin(list(self.df_prices.columns))].reset_index(drop=True)
-        df_top_borrow_positions['userDebt'] = var_simulator._calc_user_debt(df_top_borrow_positions)
+        df_top_borrow_positions['userDebt'] = self._calc_user_debt(df_top_borrow_positions)
 
         futures = []
         with ThreadPoolExecutor(CPU_COUNT * 2) as executor:
@@ -95,15 +149,35 @@ class VarSimulator:
                 self.var_results[res['sim_name']] = res['sim_result']
 
     def plot_price_trajectories(self, asset: str, n_simulations: int):
+        """
+        Plots random price trajectories for a given asset based on GARCH model simulations.
+
+        :param asset: A string indicating the name of the asset.
+        :param n_simulations: An integer indicating the number of simulations to be run.
+
+        This method generates a plot of n_simulations random price trajectories for the given asset based on GARCH model
+        simulations. It uses the `generate_random_price_trajectories` method to generate the price trajectories and plots
+        them using matplotlib.
+
+        :return: None
+        """
         plt.figure(figsize=(10, 5))
         plt.plot(np.vstack(
-            var_simulator.generate_random_price_trajectories(forecasts)[asset] for _ in range(n_simulations)).T)
+            self.generate_random_price_trajectories(self.forecasts)[asset] for _ in range(n_simulations)).T)
         plt.title(f"Random Price Trajectories for {asset}")
         plt.xlabel('Time (hours)')
         plt.ylabel('Price (USD)')
         plt.show()
 
-    def plot_var(bins: int = 30):
+    def plot_var(self, bins: int = 30):
+        """
+        Plots a histogram of the VaR estimates obtained from the simulations.
+
+        :param bins: Number of bins for the histogram.
+
+        This method plots a histogram of the VaR estimates obtained from the simulations. The `bins` parameter specifies the
+        number of bins to be used for the histogram.
+        """
         fig, ax = plt.subplots(figsize=(10, 5))
         sns.histplot(data=[v['var_value'] for _, v in self.var_results.items()], bins=bins, ax=ax)
         plt.title('Distribution of VaR Estimates')
